8d7b3c34579c3b736014e0e0cd1d50989df055de
Saleh Al Maashari <126445057+Saleh-AlMaashari@users.noreply.github.com>
2025-06-30 02:50:19 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kids-arabic-app/', // 👈 أضف هذا السطر
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
